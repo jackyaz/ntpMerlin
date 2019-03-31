96fd4243ff406b8a7eedc815c71863f9ed6e12c7
Jack <jack.naisbett@gmail.com>
2019-03-31 20:00:56 +0100

    Script header spacing

--- ntpmerlin.sh
+++ ntpmerlin.sh
@@ -470,7 +470,7 @@ ScriptHeader() {
 	printf "\\e[1m##             | |                                      ##\\e[0m\\n"
 	printf "\\e[1m##             |_|                                      ##\\e[0m\\n"
 	printf "\\e[1m##                                                      ##\\e[0m\\n"
-	printf "\\e[1m##                  %s on %-9s                ##\\e[0m\\n" "$NTPD_VERSION" "$ROUTER_MODEL"
+	printf "\\e[1m##                  %s on %-9s                 ##\\e[0m\\n" "$NTPD_VERSION" "$ROUTER_MODEL"
 	printf "\\e[1m##                                                      ##\\e[0m\\n"
 	printf "\\e[1m##       https://github.com/jackyaz/ntpMerlin           ##\\e[0m\\n"
 	printf "\\e[1m##                                                      ##\\e[0m\\n"
