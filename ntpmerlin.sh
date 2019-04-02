2a93a0ce43ecb89455c45369b47ffe96363e5bf9
Jack <jack.naisbett@gmail.com>
2019-04-02 22:11:11 +0100

    Script header alignment

--- ntpmerlin.sh
+++ ntpmerlin.sh
@@ -504,7 +504,7 @@ ScriptHeader(){
 	printf "\\e[1m##       https://github.com/jackyaz/ntpMerlin           ##\\e[0m\\n"
 	printf "\\e[1m##                                                      ##\\e[0m\\n"
 	printf "\\e[1m##                                                      ##\\e[0m\\n"
-	printf "\\e[1m##          DST is currently %-8s                       ##\\e[0m\\n" "$DST_ENABLED"
+	printf "\\e[1m##              DST is currently %-8s              ##\\e[0m\\n" "$DST_ENABLED"
 	
 	
 	printf "\\e[1m##                                                      ##\\e[0m\\n"
