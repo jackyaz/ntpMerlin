e871ddf753eaf9073f4f3c47b665c7ca38e50e57
Jack <jack.naisbett@gmail.com>
2020-04-13 18:15:12 +0100

    v2.4.1 Add unix/non-unix timestamp toggle for csv export

--- ntpmerlin.sh
+++ ntpmerlin.sh
@@ -19,7 +19,7 @@ readonly SCRIPT_NAME="ntpMerlin"
 #shellcheck disable=SC2019
 #shellcheck disable=SC2018
 readonly SCRIPT_NAME_LOWER=$(echo $SCRIPT_NAME | tr 'A-Z' 'a-z' | sed 's/d//')
-readonly SCRIPT_VERSION="v2.4.0"
+readonly SCRIPT_VERSION="v2.4.1"
 readonly SCRIPT_BRANCH="master"
 readonly SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly OLD_SCRIPT_DIR="/jffs/scripts/$SCRIPT_NAME_LOWER.d"
@@ -255,9 +255,15 @@ Conf_Exists(){
 		dos2unix "$SCRIPT_CONF"
 		chmod 0644 "$SCRIPT_CONF"
 		sed -i -e 's/"//g' "$SCRIPT_CONF"
+		if [ "$(wc -l < "$SCRIPT_CONF")" -eq 1 ]; then
+			echo "OUTPUTTIMEMODE=unix" >> "$SCRIPT_CONF"
+		fi
 		return 0
 	else
-		echo "OUTPUTDATAMODE=raw" >> "$SCRIPT_CONF"
+		{
+		echo "OUTPUTDATAMODE=raw"
+		echo "OUTPUTTIMEMODE=unix"
+		} > "$SCRIPT_CONF"
 		return 1
 	fi
 }
@@ -551,6 +557,23 @@ OutputDataMode(){
 	esac
 }
 
+OutputTimeMode(){
+	case "$1" in
+		unix)
+			sed -i 's/^OUTPUTTIMEMODE.*$/OUTPUTTIMEMODE=unix/' "$SCRIPT_CONF"
+			Generate_CSVs
+		;;
+		non-unix)
+			sed -i 's/^OUTPUTTIMEMODE.*$/OUTPUTTIMEMODE=non-unix/' "$SCRIPT_CONF"
+			Generate_CSVs
+		;;
+		check)
+			OUTPUTTIMEMODE=$(grep "OUTPUTTIMEMODE" "$SCRIPT_CONF" | cut -f2 -d"=")
+			echo "$OUTPUTTIMEMODE"
+			;;
+	esac
+}
+
 WriteStats_ToJS(){
 	echo "function $3(){" > "$2"
 	html='document.getElementById("'"$4"'").innerHTML="'
@@ -623,6 +646,7 @@ Get_ntpd_Stats(){
 
 Generate_CSVs(){
 	OUTPUTDATAMODE="$(OutputDataMode "check")"
+	OUTPUTTIMEMODE="$(OutputTimeMode "check")"
 	TZ=$(cat /etc/TZ)
 	export TZ
 	timenow=$(date +"%s")
@@ -663,7 +687,7 @@ Generate_CSVs(){
 			WriteSql_ToFile "$metric" "ntpstats" 1 7 "$CSV_OUTPUT_DIR/$metric" "weekly" "/tmp/ntp-stats.sql" "$timenow"
 			"$SQLITE3_PATH" "$SCRIPT_DIR/ntpdstats.db" < /tmp/ntp-stats.sql
 			rm -f /tmp/ntp-stats.sql
-
+			
 			WriteSql_ToFile "$metric" "ntpstats" 3 30 "$CSV_OUTPUT_DIR/$metric" "monthly" "/tmp/ntp-stats.sql" "$timenow"
 			"$SQLITE3_PATH" "$SCRIPT_DIR/ntpdstats.db" < /tmp/ntp-stats.sql
 			rm -f /tmp/ntp-stats.sql
@@ -675,7 +699,18 @@ Generate_CSVs(){
 	tmpoutputdir="/tmp/""$SCRIPT_NAME_LOWER""results"
 	mkdir -p "$tmpoutputdir"
 	cp "$CSV_OUTPUT_DIR/"*.htm "$tmpoutputdir/."
-	find "$tmpoutputdir/" -name '*.htm' -exec sh -c 'i="$1"; mv -- "$i" "${i%.htm}.csv"' _ {} \;
+	
+	if [ "$OUTPUTTIMEMODE" = "unix" ]; then
+		find "$tmpoutputdir/" -name '*.htm' -exec sh -c 'i="$1"; mv -- "$i" "${i%.htm}.csv"' _ {} \;
+	elif [ "$OUTPUTTIMEMODE" = "non-unix" ]; then
+		for i in "$tmpoutputdir/"*".htm"; do
+			awk -F"," 'NR==1 {OFS=","; print} NR>1 {OFS=","; $2=strftime("%Y-%m-%d %H:%M:%S", $2); print }' "$i" > "$i.out"
+		done
+		
+		find "$tmpoutputdir/" -name '*.htm.out' -exec sh -c 'i="$1"; mv -- "$i" "${i%.htm.out}.csv"' _ {} \;
+		rm -f "$tmpoutputdir/"*.htm
+	fi
+	
 	if [ ! -f /opt/bin/7z ]; then
 		opkg update
 		opkg install p7zip
@@ -766,10 +801,12 @@ MainMenu(){
 		NTP_REDIRECT_ENABLED="Disabled"
 	fi
 	OUTPUTDATAMODE_MENU="$(OutputDataMode "check")"
+	OUTPUTTIMEMODE_MENU="$(OutputTimeMode "check")"
 	printf "1.    Generate updated %s graphs now\\n\\n" "$SCRIPT_NAME"
 	printf "2.    Toggle redirect of all NTP traffic to %s\\n      (currently %s)\\n\\n" "$SCRIPT_NAME" "$NTP_REDIRECT_ENABLED"
 	printf "3.    Edit %s config\\n\\n" "$SCRIPT_NAME"
 	printf "4.    Toggle data output mode\\n      Currently \\e[1m%s\\e[0m values will be used for weekly and monthly charts\\n\\n" "$OUTPUTDATAMODE_MENU"
+	printf "5.    Toggle time output mode\\n      Currently \\e[1m%s\\e[0m time values will be used for CSV exports\\n\\n" "$OUTPUTTIMEMODE_MENU"
 	printf "r.    Restart ntpd\\n\\n"
 	printf "u.    Check for updates\\n"
 	printf "uf.   Update %s with latest version (force update)\\n\\n" "$SCRIPT_NAME"
@@ -808,6 +845,13 @@ MainMenu(){
 				printf "\\n"
 				if Check_Lock "menu"; then
 					Menu_ToggleOutputDataMode
+				fi
+				break
+			;;
+			5)
+				printf "\\n"
+				if Check_Lock "menu"; then
+					Menu_ToggleOutputTimeMode
 				fi
 				break
 			;;
@@ -953,6 +997,15 @@ Menu_ToggleOutputDataMode(){
 		OutputDataMode "average"
 	elif [ "$(OutputDataMode "check")" = "average" ]; then
 		OutputDataMode "raw"
+	fi
+	Clear_Lock "menu"
+}
+
+Menu_ToggleOutputTimeMode(){
+	if [ "$(OutputTimeMode "check")" = "unix" ]; then
+		OutputTimeMode "non-unix"
+	elif [ "$(OutputTimeMode "check")" = "non-unix" ]; then
+		OutputTimeMode "unix"
 	fi
 	Clear_Lock "menu"
 }
