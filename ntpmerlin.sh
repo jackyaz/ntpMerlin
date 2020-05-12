9fcc3cb6bf7ea3b87fae3d46924b25243532f2be
Jack <jack.naisbett@gmail.com>
2020-05-12 21:56:52 +0100

    Only restart dnsmasq when necessary

--- ntpmerlin.sh
+++ ntpmerlin.sh
@@ -60,10 +60,10 @@ Firmware_Version_Check(){
 ### Code for these functions inspired by https://github.com/Adamm00 - credit to @Adamm ###
 Check_Lock(){
 	if [ -f "/tmp/$SCRIPT_NAME.lock" ]; then
-		ageoflock=$(($(date +%s) - $(date +%s -r "/tmp/$SCRIPT_NAME.lock")))
+		ageoflock=$(($(date +%s) - $(date +%s -r /tmp/$SCRIPT_NAME.lock)))
 		if [ "$ageoflock" -gt 600 ]; then
 			Print_Output "true" "Stale lock file found (>600 seconds old) - purging lock" "$ERR"
-			kill "$(sed -n '1p' "/tmp/$SCRIPT_NAME.lock")" >/dev/null 2>&1
+			kill "$(sed -n '1p' /tmp/$SCRIPT_NAME.lock)" >/dev/null 2>&1
 			Clear_Lock
 			echo "$$" > "/tmp/$SCRIPT_NAME.lock"
 			return 0
@@ -368,12 +368,14 @@ Auto_DNSMASQ(){
 				if [ "$STARTUPLINECOUNTEX" -eq 0 ]; then
 					# shellcheck disable=SC2016
 					echo "dhcp-option=lan,42,$(nvram get lan_ipaddr)"' # '"$SCRIPT_NAME" >> /jffs/configs/dnsmasq.conf.add
+					service restart_dnsmasq >/dev/null 2>&1
 				fi
 			else
 				echo "" >> /jffs/configs/dnsmasq.conf.add
 				# shellcheck disable=SC2016
 				echo "dhcp-option=lan,42,$(nvram get lan_ipaddr)"' # '"$SCRIPT_NAME" >> /jffs/configs/dnsmasq.conf.add
 				chmod 0644 /jffs/configs/dnsmasq.conf.add
+				service restart_dnsmasq >/dev/null 2>&1
 			fi
 		;;
 		delete)
@@ -382,12 +384,11 @@ Auto_DNSMASQ(){
 				
 				if [ "$STARTUPLINECOUNT" -gt 0 ]; then
 					sed -i -e '/# '"$SCRIPT_NAME"'/d' /jffs/configs/dnsmasq.conf.add
+					service restart_dnsmasq >/dev/null 2>&1
 				fi
 			fi
 		;;
 	esac
-	
-	service restart_dnsmasq >/dev/null 2>&1
 }
 
 Auto_Startup(){
@@ -1434,9 +1435,7 @@ case "$1" in
 		exit 0
 	;;
 	*)
-		Check_Lock
 		echo "Command not recognised, please try again"
-		Clear_Lock
 		exit 1
 	;;
 esac
