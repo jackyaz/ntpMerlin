197447abfd32784f6adef20ca58c360b8c119e1e
Jack <jack.naisbett@gmail.com>
2021-02-09 22:02:07 +0000

    Version bump

--- ntpmerlin.sh
+++ ntpmerlin.sh
@@ -20,8 +20,8 @@
 ### Start of script variables ###
 readonly SCRIPT_NAME="ntpMerlin"
 readonly SCRIPT_NAME_LOWER=$(echo $SCRIPT_NAME | tr 'A-Z' 'a-z' | sed 's/d//')
-readonly SCRIPT_VERSION="v3.2.1"
-SCRIPT_BRANCH="master"
+readonly SCRIPT_VERSION="v3.2.2"
+SCRIPT_BRANCH="develop"
 SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME_LOWER.d"
 readonly SCRIPT_WEBPAGE_DIR="$(readlink /www/user)"
