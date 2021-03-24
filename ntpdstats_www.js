aec396bcc08ded22daac3500c698781f5de754c0
Jack Naisbett <jack.naisbett@gmail.com>
2021-03-24 22:21:06 +0000

    Remove version from page title

--- ntpdstats_www.js
+++ ntpdstats_www.js
@@ -463,7 +463,6 @@ function initial(){
 function ScriptUpdateLayout(){
 	var localver = GetVersionNumber("local");
 	var serverver = GetVersionNumber("server");
-	$j("#scripttitle").text($j("#scripttitle").text()+" - "+localver);
 	$j("#ntpmerlin_version_local").text(localver);
 	
 	if(localver != serverver && serverver != "N/A"){
