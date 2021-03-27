a3f05328f330ae964a9b127b8f00b8e56c19aa33
Jack Naisbett <jack.naisbett@gmail.com>
2021-03-27 23:30:29 +0000

    Exit code for no command supplied

--- ntpmerlin.sh
+++ ntpmerlin.sh
@@ -1774,5 +1774,6 @@ case "$1" in
 		ScriptHeader
 		Print_Output false "Command not recognised." "$ERR"
 		Print_Output false "For a list of available commands run: $SCRIPT_NAME_LOWER help"
+		exit 1
 	;;
 esac
