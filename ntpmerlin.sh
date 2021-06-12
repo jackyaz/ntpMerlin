9daff7a5c67056ce271da13c4087a245c7265f15
Jack Naisbett <jack.naisbett@gmail.com>
2021-06-12 11:28:35 +0100

    Shellcheck exclusions

--- ntpmerlin.sh
+++ ntpmerlin.sh
@@ -16,11 +16,13 @@
 
 ###############       Shellcheck directives      #############
 # shellcheck disable=SC2009
+# shellcheck disable=SC2012
 # shellcheck disable=SC2016
 # shellcheck disable=SC2018
 # shellcheck disable=SC2019
 # shellcheck disable=SC2059
 # shellcheck disable=SC2086
+# shellcheck disable=SC2155
 ##############################################################
 
 ### Start of script variables ###
